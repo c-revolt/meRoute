8370f5106c0c311532d2001d27254844ce13440b
Alexander <a.pride.xxi@tutanota.com>
2022-05-27 07:36:38 +0300

    Initial Commit

---
+++ meRoute/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meRoute
+//
+//  Created by Александр Прайд on 27.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
